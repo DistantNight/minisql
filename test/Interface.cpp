0daa7fbc8b7215c1fb28290276cce961a24eceab
DistantNight <38500980+DistantNight@users.noreply.github.com>
2018-06-19 15:00:55 +0800

    InputProcess测试用main函数

---
+++ test/Interface.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "OperationInput.h"
+#include "API.h"
+#include "InputProcess.h"
+
+using namespace std;
+
+int main()
+{
+
+	logInterface();
+	execInterface();
+	
+
+	system("pause");
+	return 0;
+}
+
