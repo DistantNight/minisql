ecdf8dbbf888d4f2d13e0f13eb843c6c8caeab2d
DistantNight <38500980+DistantNight@users.noreply.github.com>
2018-06-19 14:31:53 +0800

    更改了OperationInput.h与API.h的包含信息，加入了InputProcess模块，完成对sql语句的翻译功能（execfile todo）

---
+++ include/InputProcess.h
@@ -0,0 +1,55 @@
+#ifndef _INPUTPROCESS_H_
+#define _INPUTPROCESS_H_
+
+/*
+* operation_type:
+*     01.create table  operation
+*     02.drop   table  operation
+*     10.create index  operation
+*     11.drop   index  operation
+*     20.select        operation no where
+*     21.select        operation where
+*     30.insert        operation
+*     40.delete        operation no where
+*	  41.delete        operation where
+*     50.execfile      operation
+*     60.quit          operation
+*/
+
+using std::string;
+
+void logInterface();
+
+void execInterface();
+
+int sqlConvert(string &);
+
+int createConvert(string &);
+int createIndexConvert(string &);
+int createTableConvert(string &);
+int createTableConditionConvert(string &, CreateTable &);
+int createTablePrimaryConvert(string &, CreateTable &);
+
+int deleteConvert(string &);
+int deleteConditionConvert(string &, Delete &);
+
+int dropConvert(string &);
+int dropTableConvert(string &);
+int dropIndexConvert(string &);
+
+int insertConvert(string &);
+int insertConditionConvert(string &, Insert &);
+
+int selectConvert(string &);
+int selectConditionConvert(string &, Select &);
+
+bool isSqlEnd(string &);
+
+void removeSpace(string &);
+void removeSpaceAll(string &);
+void outputError(int opt);
+
+
+#endif // !_INPUTPROCESS_H_
+
+
---
+++ source/OperationInput.cpp
@@ -0,0 +1,6 @@
+#include "../include/OperationInput.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
