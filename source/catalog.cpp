3d76604e0982103de32629eeb49e95eeb25af98d
Lv Ming-yuan <381499740@qq.com>
2018-06-19 10:08:41 +0800

    catalog相关

---
+++ source/catalog.cpp
@@ -0,0 +1,456 @@
+#include "catalog.h"
+#include<io.h>
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+catalogManager::catalogManager() {
+	string str;
+	char temp[256];
+	ifstream f("db.catalog");
+	if (!f) {
+		cout << "error------cant open db.catlog" << endl;
+		exit(0);
+	}
+	
+	f >> tableNum;
+	f >> indexNum;
+	if (tableNum != 0) {
+		ifstream t("table.catalog");
+		if (!t) {
+			cout << "error------cant open table.catlog" << endl;
+			exit(0);
+		}
+		t >> temp;
+		while (strcmp(temp, "!end") != 0) {
+			if (strcmp(temp, "!start") == 0 ) {
+				t >> temp;
+				
+				catalogManager::table Ta;
+				Ta.nameOfTable.assign(temp);
+				t >> Ta.numberOfKey;
+				for (int i = 0; i < Ta.numberOfKey; i++) {
+					t >> temp;
+					str.assign(temp);
+					Ta.nameOfKey.push_back(str);
+				}
+				for (int i = 0; i < Ta.numberOfKey; i++) {
+					t >> temp;
+					str.assign(temp);
+					Ta.nameOfType.push_back(str);
+				}
+				for (int i = 0; i < Ta.numberOfKey; i++) {
+					int x;
+					t >> x;
+					Ta.isUnique.push_back(x);
+				}
+				
+			for (int i = 0; i < Ta.numberOfKey; i++) {
+					t >> temp;
+					str.assign(temp);
+					Ta.nameOfIndex.push_back(str);
+				}
+				int x;
+				t >> x;
+				Ta.numberOfRecord = x;
+				Ta.lengthOfRecord = getLength(Ta.nameOfTable);
+				t >> temp;
+				Ta.primaryKey.assign(temp);
+
+
+				myt.push_back(Ta);
+
+
+
+			}
+
+			t >> temp;
+		}
+		if (indexNum != 0) {
+			ifstream i("index.catalog");
+			if (!i) {
+				cout << "error------cant open index.catlog" << endl;
+				exit(0);
+			}
+			i >> temp;
+			while (strcmp(temp, "!end") != 0) {
+				if (strcmp(temp, "!start") == 0) {
+					catalogManager::index In;
+					i >> temp;
+					In.nameOfIndex.assign(temp);
+					i >> In.tableOfIndex;
+					i >> In.keyOfIndex;
+
+				}
+
+				i >> temp;
+			}
+		}
+	}
+
+
+
+}
+
+
+
+ 
+ int catalogManager::isTableExist(string tablename) {
+	 vector<catalogManager::table>::iterator s;
+	 for (s = myt.begin(); s != myt.end(); ++s) {
+		 if ((*s).nameOfTable == tablename) return 1;
+		 
+	 }return 0;
+ }
+ int catalogManager::isIndexExist(string indexname) {
+	 vector<catalogManager::index>::iterator s;
+	 for (s = myi.begin(); s != myi.end(); ++s) {
+		 if ((*s).nameOfIndex == indexname) return 1;
+
+	 }return 0;
+ }
+ int catalogManager::myprint(string tablename) {
+	 if (isTableExist(tablename) == 0) {
+		 cout << "this table dosent exist" << endl;
+		 return 0;
+	 }
+	 else {
+		 vector<catalogManager::table>::iterator s;
+		 for (s = myt.begin(); s != myt.end(); ++s) {
+			 if ((*s).nameOfTable == tablename) {
+				 cout << "table's name:" << (*s).nameOfTable << endl;
+				 cout << "keys:";
+					 for (int i = 0; i < (*s).numberOfKey; i++) {
+						 cout << (*s).nameOfKey[i] << " ";
+					 }cout << " " << endl;
+					 cout << "unique:";
+					 for (int i = 0; i < (*s).numberOfKey; i++) {
+						 cout << (*s).isUnique[i] << " ";
+					 }cout << " " << endl;
+					 cout << "primary key:" << (*s).primaryKey << endl;
+					 cout << "number of record:" << (*s).numberOfRecord << endl;
+					 cout << "index:";
+					 for (int i = 0; i < (*s).numberOfKey; i++) {
+						 cout << (*s).nameOfIndex[i] << " ";
+					 }cout << " " << endl;
+
+			 }
+
+		 }
+	 }return 0;
+ }
+ bool catalogManager::deleteTableInfo(string nameOfTable){
+ 
+	 return deleteTableInfo(nameOfTable, getRecordNum(nameOfTable));
+ 
+ }
+ bool catalogManager::deleteTableInfo(string nameOfTable,int d) {
+	 int i = 0;
+	 for (i; i < myt.size(); i++) {
+		 if (myt[i].nameOfTable == nameOfTable) {
+			 myt[i].numberOfRecord = myt[i].numberOfRecord - d;
+			 return 1;
+		 }
+	 }
+	 return 0;
+ }
+ bool catalogManager::dropTable(string nameOfTable) {
+
+	 int flag = 0;
+	 tableNum--;                                   
+	 vector<catalogManager::table>::iterator p;
+	 for (p = myt.begin(); p != myt.end(); ++p)
+	 {
+		 if ((*p).nameOfTable == nameOfTable)
+		 {
+			 
+			 for (unsigned i = 0; i<(*p).nameOfIndex.size(); i++)
+				 if ((*p).nameOfIndex.at(i) != "*")
+					deleteIndex((*p).nameOfIndex.at(i));
+			
+			 p= myt.erase(p);
+			 flag = 1;
+			 break;
+		 }
+	 }
+	 if (flag == 0)                
+	 {
+		 cout  << nameOfTable << " doesnt exists" << endl;
+		 return 0;
+	 }
+
+	 remove(nameOfTable.c_str());
+
+
+	 return 1;
+ }
+
+ bool catalogManager::createIndex(string nameOfIndex, string tableOfIndex, string keyOfIndex) {
+	 vector<catalogManager::table>::iterator s;
+	 for (s = myt.begin(); s != myt.end(); ++s)
+	 {
+		 if ((*s).nameOfTable== tableOfIndex)
+		 {
+			 unsigned int i = 0;
+			 for (i = 0; i<(*s).nameOfKey.size(); i++)
+			 {
+				 if ((*s).nameOfKey[i] == keyOfIndex)
+				 {
+					 
+					 if (strcmp((*s).nameOfIndex[i].c_str(), "*") != 0)
+					 {
+						 cout << "The index already exists" << endl;
+						 return 0;
+					 }
+					 (*s).nameOfIndex[i] = nameOfIndex;
+					 break;
+				 }
+			 }
+			 if (i == (*s).nameOfKey.size())
+			 {
+				 cout << "Fail to create index" << endl;
+				 return 0;
+			 }
+			 break;
+		 }
+	 }
+	 if (s == myt.end())
+	 {
+		 cout << "Fail to create index" << endl;
+		 return 0;
+	 }
+
+	 
+	 catalogManager::index myt;
+	myt.nameOfIndex = nameOfIndex;
+	 myt.tableOfIndex = tableOfIndex;
+	myt.keyOfIndex = keyOfIndex;
+	 myi.push_back(myt);
+	 indexNum++;
+
+	 
+ }
+ bool catalogManager::deleteIndex(string nameOfIndex) {
+	 indexNum = indexNum - 1;
+	 int flag = 0;
+	 vector<catalogManager::index>::iterator x;
+	 for (x = myi.begin(); x != myi.end(); x++) {
+		 if ((*x).nameOfIndex == nameOfIndex) {
+			 for (int i = 0; i < myt.size(); i++) {
+				 if (myt[i].nameOfTable == (*x).tableOfIndex) {
+					 for (int j = 0; j < myt[i].nameOfIndex.size(); j++) {
+						 if (myt[i].nameOfKey[j] == (*x).keyOfIndex) {
+							 myt[i].nameOfIndex[j] = "*";
+								 break;
+						 }
+					 }
+					 break;
+				 }
+			 }
+
+			 x = myi.erase(x);
+			 flag = 1;
+			 break;
+		 }
+
+}
+	 if (flag != 1) {
+		 cout << nameOfIndex << " dosent exists" << endl;
+}
+	 remove(nameOfIndex.c_str());
+	 return 1;
+
+ }
+ bool catalogManager::numberOfRecordAdd(string nameOfTable, int a) {
+	 int flag = 0;
+	 vector<catalogManager::table>::iterator x;
+	 for (x = myt.begin(); x != myt.end(); x++) {
+		 if ((*x).nameOfTable == nameOfTable) {
+			 flag = 1;
+			 (*x).numberOfRecord = (*x).numberOfRecord + a;
+		 }
+	 }
+	 if (flag == 0) {
+		 cout << "insert recorf fall" << endl;
+		 return 0;
+	 }
+	 return 1;
+ }
+ 
+
+ bool catalogManager::updateCatalog() {
+	const char * fileName = "db.catalog";
+	ofstream file(fileName);
+	if (!file) {
+		cout << "cant write into db.catalog" << endl;
+	}
+	else {
+		file << tableNum << endl;
+		file << indexNum << endl;
+	}
+	const char * fileName2 = "index.catalog";
+	ofstream file2(fileName2);
+	if (!file2) {
+		cout << "cant write into index.catalog" << endl;
+	}
+	for (int i = 0; i < indexNum; i++) {
+		file2 << "!start" << endl;
+		file2 << myi[i].nameOfIndex << endl;
+		file2 << myi[i].tableOfIndex<<" "<<myi[i].keyOfIndex << " "<<endl;
+	}   file2 << "!end" << endl;
+	const char * fileName3 = "table.catalog";
+	ofstream file3(fileName3);
+	if (!file3) {
+		cout << "cant write into table.catalog" << endl;
+	}
+	for (int i = 0; i < tableNum; i++) {
+		file3 << "!start" << endl;
+		file3<< myt[i].nameOfTable << endl;
+		file3 << myt[i].numberOfKey << endl;
+		for (int j = 0; j<myt[i].numberOfKey; j++)
+			file3 << myt[i].nameOfKey[j] << " ";
+		file3 << endl;
+		for (int j = 0; j<myt[i].numberOfKey; j++)
+			file3 << myt[i].nameOfType[j] << " ";
+		file3 << endl;
+		for (int j = 0; j<myt[i].numberOfKey; j++)
+			file3 << myt[i].isUnique[j] << " ";
+		file3 << endl;
+		for (int j = 0; j<myt[i].numberOfKey; j++)
+			file3 << myt[i].nameOfIndex[j] << " ";
+		file3 << endl;
+		
+		file3 << myt[i].numberOfRecord << endl;
+		file3 << myt[i].lengthOfRecord << endl;
+		file3 << myt[i].primaryKey<< endl;
+	}
+	file3 << "!end" << endl;
+
+
+ }
+ int catalogManager::getFullLength(string nameOfTable) {
+	 int value;
+	 value = 0;
+	 vector<string> a;
+	 vector<catalogManager::table>::const_iterator b;
+	 for (b = myt.begin(); b != myt.end(); b++) {
+		 if ((*b).nameOfTable == nameOfTable)
+			 a = (*b).nameOfType;
+	 }
+	 value = getLength(a);
+ }
+ int catalogManager::getLength(vector<string> x) {
+	 int value = 0;
+	 vector<string>::const_iterator a;
+	 for (a = x.begin(); a != x.end(); a++) {
+		 value = value + getLength(*a);
+	 }return value;
+ }
+ int catalogManager::getLength(string type) {
+	 const char * str;
+	 str = type.c_str();
+	 int x;
+	 if (strcspn(str, "int") == 0) {
+		 x = sizeof(int);
+	 }
+	 else if (strcspn(str, "char") == 0) {
+		 const char * p1, *p2;
+		 char p[5];
+		 int n;
+		 p1 = strstr(str, "(");
+		 p2 = strstr(str, ")");
+		 p1++; 
+		 strcpy(p, p1);
+		 p[p2 - p1] = 0;
+		 n = atoi(p);
+		 x = n * sizeof(char);
+	 }
+	 else if (strcspn(str, "float") == 0)      
+	 {
+		 x = sizeof(float);
+	 }
+	 else
+	 {
+		 cout << "cant get length of type" << endl;
+		 return -1;
+	 }
+	 return x;
+
+
+
+
+ }
+ 
+ vector<string> catalogManager::nameOfKey(string nameOfTable) {
+	 vector<catalogManager::table>::const_iterator x;
+	 vector<string> a;
+	 for (x = myt.begin(); x != myt.end(); x++) {
+		 if ((*x).nameOfTable == nameOfTable)
+			 return (*x).nameOfKey;
+	 }return a;
+ }
+ vector<string> catalogManager::nameOfType(string nameOfTable) {
+	 vector<catalogManager::table>::const_iterator x;
+	 vector<string> a;
+	 for (x = myt.begin(); x != myt.end(); x++) {
+		 if ((*x).nameOfTable == nameOfTable)
+			 return (*x).nameOfType;
+	 }return a;
+ }
+ vector<int> catalogManager::isUnique(string nameOfTable) {
+	 vector<catalogManager::table>::const_iterator x;
+	 vector<int> a;
+	 for (x = myt.begin(); x != myt.end(); x++) {
+		 if ((*x).nameOfTable == nameOfTable)
+			 return (*x).isUnique;
+	 }return a;
+ }
+ vector<string> catalogManager::nameOfIndex(string nameOfTable) {
+	 vector<catalogManager::table>::const_iterator x;
+	 vector<string> a;
+	 for (x = myt.begin(); x != myt.end(); x++) {
+		 if ((*x).nameOfTable == nameOfTable)
+			 return (*x).nameOfIndex;
+	 }return a;
+ }
+
+ int catalogManager::getRecordNum(string tableName) {
+	 int num = 0;
+	 for (int i = 0; i < myt.size(); i++) {
+		 if (myt.at(i).nameOfTable == tableName) {
+			 num = myt.at(i).numberOfRecord;
+			 break;
+		 }
+	 }return num;
+ }
+bool catalogManager::createTable(string nameOfTable, vector<string> nameOfKey, vector<string> nameOfType, vector<int> isUnique) {
+	string x("*");
+	if (createTable(nameOfTable, nameOfKey, nameOfType, isUnique, x)) return 1;
+	else return 0;
+
+}
+bool catalogManager::createTable(string nameOfTable, vector<string> nameOfKey, vector<string> nameOfType, vector<int> isUnique, string primaryKey) {
+	if (isTableExist(nameOfTable) == 1) {
+		cout << "this table has already exists" << endl;
+		return 0;
+	}
+	else {
+		tableNum++;
+		catalogManager::table x;
+		x.nameOfTable = nameOfTable;
+		x.numberOfKey = nameOfKey.size();
+		x.nameOfKey = nameOfKey;
+		x.nameOfType = nameOfType;
+		x.isUnique = isUnique;
+		x.numberOfRecord = 0;
+		x.lengthOfRecord = getLength(nameOfType);
+		x.primaryKey = primaryKey;
+		for (int i = 0; i < nameOfKey.size(); i++) {
+			x.nameOfIndex.push_back("*");
+		}
+		myt.push_back(x);
+		return 1;
+	}
+		
+
+}
---
+++ source/catalogManager.cpp
@@ -0,0 +1,124 @@
+#include "API.h"
+#include "catalog.h"
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+class catalogManager;
+extern catalogManager x;
+
+bool getTableInfo(Table & a) {
+
+	x.myprint(a.table_name);
+}
+string catalogExecute(CreateTable & b) {
+	vector<string> colname;
+	for (int i = 0; i < b.column_name->length; i++) {
+		colname.push_back(b.column_name[i]);
+	}
+	vector<string> coltype;
+	for (int i = 0; i < b.column_num; i++) {
+		//coltype.push_back(b.column_type[i]);
+		if (b.column_type[i] == INT) {
+			coltype.push_back("int");
+		}
+		else if (b.column_type[i] == CHAR) {
+			coltype.push_back("char");
+		}
+		else if (b.column_type[i] == FLOAT) {
+			coltype.push_back("float");
+		}
+	}
+	vector<int> isU;
+	for (int i = 0; i < b.column_num; i++) {
+		isU[i] = (int)b.is_unique[i];
+	}
+	bool j = x.createTable(b.table_name, colname, coltype, isU, b.primary_key);
+	if (j == 0) {
+		cout << "create fail" << endl;
+		return 0;
+	}
+	cout << "creatr success" << endl;
+	//x.updateCatalog();
+	return 0;
+}
+
+string catalogExecute(DropTable & c) {
+	int judge = x.isTableExist(c.table_name);
+	if (judge == 0) {
+		cout << "no table" << endl;
+		return 0;
+	}
+	x.dropTable(c.table_name);
+	cout << "drop success" << endl;
+	return 0;
+}
+bool catalogExecute(CreateIndex &d) {
+	int judge = x.isIndexExist(d.index_name);
+	if (judge == 1) {
+		cout << "index exist" << endl;
+		return 0;
+	}
+	x.createIndex(d.index_name, d.table_name, d.index_column_name);
+	cout << "create index successfully" << endl;
+	return 1;
+
+}
+bool catalogExecute(DropIndex &e) {
+	int judge = x.isIndexExist(e.index_name);
+	if (judge = 0) {
+		cout << "index not exist" << endl;
+		return 0;
+	}
+	x.deleteIndex(e.index_name);
+	cout << "delete successfully" << endl;
+	return 1;
+
+}
+bool catalogExecute(Insert &f) {
+	int j = x.isTableExist(f.table_name);
+	if (j == 0) {
+		cout << "insert error" << endl;
+		return 0;
+	}
+	return x.numberOfRecordAdd(f.table_name, 1);
+}
+bool catalogExecute(Delete &g) {
+	int a = x.isTableExist(g.table_name);
+	if (a == 0) {
+		cout << "no table" << endl;
+		return 0;
+	}
+	if (x.deleteTableInfo(g.table_name, 1)) {
+		cout << "delete success" << endl;
+		return 1;
+	}
+	else {
+		cout << "fail to delete" << endl;
+		return 0;
+	}
+}
+bool catalogExecute(Select &h) {
+	int a = x.isTableExist(h.table_name);
+	if (a == 0) {
+		cout << "table not exist" << endl;
+		return 0;
+	}
+	int flag = 0;
+	vector<string> s = x.nameOfKey(h.table_name);
+	if (h.condition_name[0] == "*") {
+		cout << "select success" << endl;
+		return 1;
+	}
+	for (int i = 0; i < s.size(); i++) {
+		if (h.condition_name[0] == s[i]) flag = 1;
+	}
+	if (flag == 1) {
+		cout << "select success" << endl;
+		return 1;
+	}
+	else {
+		cout << "select error" << endl;
+		return 0;
+	}
+}
