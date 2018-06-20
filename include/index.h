d186032408e9e7d3bb13ccf4fa30ddea611c5246
zproizuko <37054633+zproizuko@users.noreply.github.com>
2018-06-20 12:03:02 +0800

    index头文件修改

--- include/index.h
+++ include/index.h
@@ -8,7 +8,6 @@
 #include "BPTree.h"
 #include "buffer.h"
 
-template <typename T> 
 class IndexManager{
 	public:
 		IndexManager(const Table& table);
@@ -19,9 +18,13 @@ class IndexManager{
 		//删除索引
 		//输入：索引名，属性类型
 		void 		dropIndex(std::string file_path, COLUMNTYPE type); 
+		template 	<typename T> 
 		int 		findIndex(std::string file_path, T data, COLUMNTYPE type);
+		template 	<typename T> 
 		void 		insertIndex(std::string file_path, T data, COLUMNTYPE type, int block_id);
+		template 	<typename T> 
 		void 		deleteIndexByKey(std::string file_path, T data, COLUMNTYPE type);
+		template 	<typename T> 
 		void 		searchWithinRange(std::string file_path, T dataA, T dataB, COLUMNTYPE type, std::vector<int>& values, int flag);
 	
 	private:
@@ -35,5 +38,129 @@ class IndexManager{
 		
 		int 		getBestDegree(COLUMNTYPE type, int string_len);
 };
+
+template <typename T> 
+int IndexManager::findIndex(std::string file_path, T data, COLUMNTYPE type)
+{
+	if(type == INT)
+	{
+		intMap::iterator i_Int = indexIntMap.find(file_path);
+		if(i_Int == indexIntMap.end())
+		{
+			return -1;
+		}
+		else return i_Int -> second -> searchValueWithKey(data);
+	}
+	else if(type == FLOAT)
+	{
+		floatMap::iterator i_Float = indexFloatMap.find(file_path);
+		if(i_Float == indexFloatMap.end())
+		{
+			return -1;
+		}
+		else return i_Float -> second -> searchValueWithKey(data);
+	}
+	else
+	{
+		stringMap::iterator i_String = indexStringMap.find(file_path);
+		if(i_String == indexStringMap.end())
+		{
+			return -1;
+		}
+		else return i_String -> second -> searchValueWithKey(data);
+	}
+}
+
+template <typename T>
+void IndexManager::insertIndex(std::string file_path, T data, COLUMNTYPE type, int block_id)
+{
+	if(type == INT)
+	{
+		intMap::iterator i_Int = indexIntMap.find(file_path);
+		if(i_Int == indexIntMap.end())
+		{
+			return;
+		}
+		else i_Int -> second -> insertKey(data, block_id);
+	}
+	else if(type == FLOAT)
+	{
+		floatMap::iterator i_Float = indexFloatMap.find(file_path);
+		if(i_Float == indexFloatMap.end())
+		{
+			return;
+		}
+		else i_Float -> second -> insertKey(data, block_id);
+	}
+	else
+	{
+		stringMap::iterator i_String = indexStringMap.find(file_path);
+		if(i_String == indexStringMap.end())
+		{
+			return;
+		}
+		else i_String -> second -> insertKey(data, block_id);
+	}
+	return;
+}
+
+template <typename T>
+void IndexManager::deleteIndexByKey(std::string file_path, T data, COLUMNTYPE type)
+{
+	if(type == INT)
+	{
+		intMap::iterator i_Int = indexIntMap.find(file_path);
+		if(i_Int == indexIntMap.end())
+		{
+			return;
+		}
+		else i_Int -> second -> deleteKey(data);
+	}
+	else if(type == FLOAT)
+	{
+		floatMap::iterator i_Float = indexFloatMap.find(file_path);
+		if(i_Float == indexFloatMap.end())
+		{
+			return;
+		}
+		else i_Float -> second -> deleteKey(data);
+	}
+	else
+	{
+		stringMap::iterator i_String = indexStringMap.find(file_path);
+		if(i_String == indexStringMap.end())
+		{
+			return;
+		}
+		else i_String -> second -> deleteKey(data);
+	}
+	return;
+}
+ 
+
+
+//给定文件名，数据类型的范围查找，dataA与dataB大小关系无所谓，flag = 0是默认，1是无视dataA，返回>dataB的结果，2是无视dataB 
+template <typename T>
+void IndexManager::searchWithinRange(std::string file_path, T dataA, T dataB, COLUMNTYPE type, std::vector<int>& values, int flag)
+{
+    if(type == INT) 
+	{
+        intMap::iterator itInt = indexIntMap.find(file_path);
+        if(itInt == indexIntMap.end()) return;
+        else itInt -> second -> searchInRange(dataA, dataB, values, flag);
+    } 
+	else if(type == FLOAT) 
+	{
+        floatMap::iterator itFloat = indexFloatMap.find(file_path);
+        if (itFloat == indexFloatMap.end()) return;
+        else itFloat -> second -> searchInRange(dataA, dataB, values, flag);
+    } 
+	else 
+	{
+        stringMap::iterator itString = indexStringMap.find(file_path);
+        if(itString == indexStringMap.end()) return;
+        else itString -> second -> searchInRange(dataA, dataB, values, flag);
+    }
+}
 
 #endif
