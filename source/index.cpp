e22de52cb7db6b67236d044784c1a76d90be5505
zproizuko <37054633+zproizuko@users.noreply.github.com>
2018-06-20 12:03:26 +0800

    index源文件修改

--- source/index.cpp
+++ source/index.cpp
@@ -7,8 +7,7 @@
 #include <vector>
 #include <map>
 
-template <typename T> 
-IndexManager<T>::IndexManager(const Table& table)
+IndexManager::IndexManager(const Table& table)
 {
 	//catalogManager catalog;
 	//if(catalog.isTableExist(Table.table_name))
@@ -25,10 +24,9 @@ IndexManager<T>::IndexManager(const Table& table)
 	//}	
 } 
 
-template <typename T> 
-IndexManager<T>::~IndexManager()
+IndexManager::~IndexManager()
 {
-	for(intMap::iterator i_Int = indexIntMap.begin(); i_Int != indexIntMap.end(); ++i_Int)
+	for(intMap::iterator i_Int = indexIntMap.begin(); i_Int != indexIntMap.end(); i_Int++)
 	{
 		if(i_Int -> second)
 		{
@@ -36,7 +34,7 @@ IndexManager<T>::~IndexManager()
 			delete i_Int -> second;
 		}
 	}
-	for(floatMap::iterator i_Float = indexFloatMap.begin(); i_Float != indexFloatMap.end(); ++i_Float)
+	for(floatMap::iterator i_Float = indexFloatMap.begin(); i_Float != indexFloatMap.end(); i_Float++)
 	{
 		if(i_Float -> second)
 		{
@@ -44,7 +42,7 @@ IndexManager<T>::~IndexManager()
 			delete i_Float -> second;
 		}
 	}
-	for(stringMap::iterator i_String = indexStringMap.begin(); i_String != indexStringMap.end(); ++i_String)
+	for(stringMap::iterator i_String = indexStringMap.begin(); i_String != indexStringMap.end(); i_String++)
 	{
 		if(i_String -> second)
 		{
@@ -54,8 +52,7 @@ IndexManager<T>::~IndexManager()
 	}	
 }
 
-template <typename T> 
-void IndexManager<T>::createIndex(std::string file_path, COLUMNTYPE type, int string_len)
+void IndexManager::createIndex(std::string file_path, COLUMNTYPE type, int string_len)
 {
 	int degree = getBestDegree(type, string_len);
 	
@@ -77,8 +74,7 @@ void IndexManager<T>::createIndex(std::string file_path, COLUMNTYPE type, int st
 	return;
 }
 
-template <typename T> 
-void IndexManager<T>::dropIndex(std::string file_path, COLUMNTYPE type)
+void IndexManager::dropIndex(std::string file_path, COLUMNTYPE type)
 {
 	if(type == INT)
 	{
@@ -122,133 +118,10 @@ void IndexManager<T>::dropIndex(std::string file_path, COLUMNTYPE type)
 	return;
 }
 
-template <typename T> 
-int IndexManager<T>::findIndex(std::string file_path, T data, COLUMNTYPE type)
+int IndexManager::getBestDegree(COLUMNTYPE type, int string_len)
 {
-	if(type == INT)
-	{
-		intMap::iterator i_Int = indexIntMap.find(file_path);
-		if(i_Int == indexIntMap.end())
-		{
-			return -1;
-		}
-		else return i_Int -> second -> searchValueWithKey(data);
-	}
-	else if(type == FLOAT)
-	{
-		floatMap::iterator i_Float = indexFloatMap.find(file_path);
-		if(i_Float == indexFloatMap.end())
-		{
-			return -1;
-		}
-		else return i_Float -> second -> searchValueWithKey(data);
-	}
-	else
-	{
-		stringMap::iterator i_String = indexStringMap.find(file_path);
-		if(i_String == indexStringMap.end())
-		{
-			return -1;
-		}
-		else return i_String -> second -> searchValueWithKey(data);
-	}
-}
-
-template <typename T>
-void IndexManager<T>::insertIndex(std::string file_path, T data, COLUMNTYPE type, int block_id)
-{
-	if(type == INT)
-	{
-		intMap::iterator i_Int = indexIntMap.find(file_path);
-		if(i_Int == indexIntMap.end())
-		{
-			return;
-		}
-		else i_Int -> second -> insertKey(data, block_id);
-	}
-	else if(type == FLOAT)
-	{
-		floatMap::iterator i_Float = indexFloatMap.find(file_path);
-		if(i_Float == indexFloatMap.end())
-		{
-			return;
-		}
-		else i_Float -> second -> insertKey(data, block_id);
-	}
-	else
-	{
-		stringMap::iterator i_String = indexStringMap.find(file_path);
-		if(i_String == indexStringMap.end())
-		{
-			return;
-		}
-		else i_String -> second -> insertKey(data, block_id);
-	}
-	return;
-}
-
-template <typename T>
-void IndexManager<T>::deleteIndexByKey(std::string file_path, T data, COLUMNTYPE type)
-{
-	if(type == INT)
-	{
-		intMap::iterator i_Int = indexIntMap.find(file_path);
-		if(i_Int == indexIntMap.end())
-		{
-			return;
-		}
-		else i_Int -> second -> deleteKey(data);
-	}
-	else if(type == FLOAT)
-	{
-		floatMap::iterator i_Float = indexFloatMap.find(file_path);
-		if(i_Float == indexFloatMap.end())
-		{
-			return;
-		}
-		else i_Float -> second -> deleteKey(data);
-	}
-	else
-	{
-		stringMap::iterator i_String = indexStringMap.find(file_path);
-		if(i_String == indexStringMap.end())
-		{
-			return;
-		}
-		else i_String -> second -> deleteKey(data);
-	}
-	return;
-}
-
-template <typename T> 
-int IndexManager<T>::getBestDegree(COLUMNTYPE type, int string_len)
-{	
 	int degree = (BLOCK_LEN - sizeof(int)) / (string_len + sizeof(int));
 	if(degree % 2 == 0) degree--;
 	return degree;
-}
-
-//给定文件名，数据类型的范围查找，dataA与dataB大小关系无所谓，flag = 0是默认，1是无视dataA，返回>dataB的结果，2是无视dataB 
-template <typename T>
-void IndexManager<T>::searchWithinRange(std::string file_path, T dataA, T dataB, COLUMNTYPE type, std::vector<int>& values, int flag)
-{
-    if(type == INT) 
-	{
-        intMap::iterator itInt = indexIntMap.find(file_path);
-        if(itInt == indexIntMap.end()) return;
-        else itInt -> second -> searchInRange(dataA, dataB, values, flag);
-    } 
-	else if(type == FLOAT) 
-	{
-        floatMap::iterator itFloat = indexFloatMap.find(file_path);
-        if (itFloat == indexFloatMap.end()) return;
-        else itFloat -> second -> searchInRange(dataA, dataB, values, flag);
-    } 
-	else 
-	{
-        stringMap::iterator itString = indexStringMap.find(file_path);
-        if(itString == indexStringMap.end()) return;
-        else itString -> second -> searchInRange(dataA, dataB, values, flag);
-    }
 }
 
